c8caddcc66d3d0f9fe25b581c0cf32d75023ed7a
Skorohod Andrew <pmisa376@gmail.com>
2022-07-17 19:22:55 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const listFoto = document.querySelector('.gallery');
+const cardsFoto = createFotoCard(galleryItems);
+
+listFoto.insertAdjacentHTML('beforeend', cardsFoto);
+
+listFoto.addEventListener('click', onListFotoClick);
+
+
+function createFotoCard (fotos){
+    return fotos.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img class="gallery__image" 
+                src="${preview}" 
+                data-source="${original}" 
+                alt="${description}" />
+            </a>
+        </div> 
+    `;
+})
+.join('');
+
+
+}
+
+function onListFotoClick (evt){
+    evt.preventDefault()
+    if (!evt.target.classList.contains('gallery__image')){
+        return
+    }
+
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">`)
+
+    instance.show()
+
+}
+
+
+// console.log(createFotoCard(galleryItems));
+
+// console.log(galleryItems);
